aa36b2a0dee08cd6f69ccf3f25fd915f3d269cba
nexo <130805081+Nexoiguazu@users.noreply.github.com>
2026-01-22 23:18:42 -0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-(function(){
-  var el = document.getElementById('updated');
-  if(el){
-    el.textContent = new Date().toLocaleDateString('es-AR', { year:'numeric', month:'long', day:'numeric' });
-  }
-})();
